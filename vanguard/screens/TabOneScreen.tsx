75aede20a5e6bc20b6816de8e7ba1b109a82068c
vyomajani <vyomajani@gmail.com>
2021-04-11 00:45:12 -0400

    Removing unnecessary comments

--- vanguard/screens/TabOneScreen.tsx
+++ vanguard/screens/TabOneScreen.tsx
@@ -37,7 +37,7 @@ export default class TabOneScreen extends React.Component {
       <View style={styles.container}>
         <Text style={styles.title}>Tab One</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
       </View>
     );
   }
--- vanguard/screens/TabThreeScreen.tsx
+++ vanguard/screens/TabThreeScreen.tsx
@@ -13,7 +13,7 @@ export default function TabThreeScreen() {
         <View style={styles.separator} 
             lightColor="#eee" 
             darkColor="rgba(255,255,255,0.1)" />
-        <EditScreenInfo path="/screens/TabThreeScreen.tsx" />
+        {/* <EditScreenInfo path="/screens/TabThreeScreen.tsx" /> */}
       </View>
     );
   }
