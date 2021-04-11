df4352e968280ec92b8d5cd46422285ac277be7a
Ferzam Mohammad <ferzammohammad99@gmail.com>
2021-04-10 20:53:08 -0400

    Playing around with changes; nothing super helpful

--- vanguard/screens/TabOneScreen.tsx
+++ vanguard/screens/TabOneScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
+import MapView from 'react-native-maps';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
@@ -8,6 +8,14 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
